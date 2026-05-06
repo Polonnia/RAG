feb0addb4b30dc6b300dfc9eaf8ff6355ad564b4
jgjhbk <3142900739@qq.com>
2026-05-06 17:02:59 +0800

    fix: OpenAvatarChat 正常作为子模块使用

--- frontend/src/api/teachingApi.js
+++
@@ -1,8 +0,0 @@
-import http from './http';
-
-export const generateTeachingDesign = (payload) => http.post('/teaching/design', payload).then(r => r.data);
-export const generatePPT = (payload) => http.post('/teaching/ppt', payload).then(r => r.data);
-
-export default { generateTeachingDesign, generatePPT };
-
-
--- frontend/src/index.js
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import AppRoutes from './routes';
-import 'antd/dist/reset.css';
-import './global.css';
-
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(<AppRoutes />);
